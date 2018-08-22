4e98ee926da8c13c856d7c881d04e6d888d87291
Thomas Shehan <thomas.shehan88@gmail.com>
2018-08-22 18:34:24 -0500

    build tests framework

--- test/test-server.js
+++ test/test-server.js
@@ -1,4 +1,4 @@
-const chai = requre('chai');
+const chai = require('chai');
 const chaitHttp = require('chai-http');
 const faker = require('faker');
 const mongoose = require('mongoose');
@@ -12,5 +12,67 @@ const {TEST_DATABASE_URL} = require('../config');
 chai.use(chaitHttp);
 
 function seedBlogPostData() {
-  console.info('')
+  console.info('seeding post data');
+  const seedData = [...Array(10)].map(() => generateBlogPostData());
+  return BlogPost.insertMany(seedData);
 }
+
+function generateBlogPostData() {
+  return {
+    author: {
+      firstName: faker.name.firstName(),
+      lastName: faker.name.lastName(),
+    },
+    title: faker.random.words(),
+    content: faker.lorem.paragraph(),
+    created: faker.date.past(),
+  };
+}
+
+function tearDownDb() {
+  console.warn('Deleting database');
+  return mongoose.connection.dropDatabase();
+}
+
+describe('BlogPost API Resource', function() {
+
+  before(function() {
+    return runServer(TEST_DATABASE_URL);
+  });
+
+  beforeEach(function() {
+    return seedBlogPostData();
+  });
+
+  afterEach(function() {
+    return tearDownDb();
+  });
+
+  after(function() {
+    return closeServer();
+  });
+
+  describe('GET /posts endpoint', function() {
+
+  });
+
+  describe('GET posts/:id endpoint', function() {
+
+  });
+
+  describe('POST /posts endpoint', function() {
+
+  });
+
+  describe('DELETE /posts/:id endpoint', function() {
+
+  });
+
+  describe('PUT /posts/:id endpoint', function() {
+
+  });
+
+  describe('DELETE /:id endpoint', function() {
+
+  });
+});
